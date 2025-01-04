a06bc2b19ff7f9b14e6a1880fa17849ca63bd246
Marcus Berge (Mac) <marcus.linna.berge@gmail.com>
2025-01-04 16:04:20 +0100

    Make first game files

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
+import pygame
 
+from game import Game
+from settings import Settings
 
 class Main:
     def __init__(self) -> None:
-        pass
+        pygame.display.set_mode((Settings.display.WIDTH, Settings.display.HEIGHT))
+
+        self.game = Game()
+        
+    def run(self) -> None:
+        self.game.run()
+
+
+if __name__ == "__main__":
+    main = Main()
+    main.run()
 
---
+++ settings.py
@@ -0,0 +1,9 @@
+
+
+class Display_Settings:
+    WIDTH: int = 1200
+    HEIGHT: int = 750
+    FPS: float = 60.0
+
+class Settings:
+    display = Display_Settings()
